c5c6985a4d9f06185678883931912d1d95d254f6
David Azcona <dazcona@etsy.com>
2024-06-29 18:57:43 +0100

    get audio

--- app.py
+++ app.py
@@ -50,29 +50,42 @@ def get_audio(story_id, branch_id):
 
 
 @app.route("/v1/story/<story_id>/branch/<branch_id>/", methods=("GET", "POST"))
-def stories(story_id, branch_id):
+def post_and_get_audio(story_id, branch_id):
     """Get and post story"""
 
     if request.method == "POST":
-        # Get input from user: text, + or -
-        input = request.form["input"]
-        # Already in DB?
-        query = """
-        SELECT story 
-        FROM story 
-        WHERE id = '{story_id}'
+        # Get input from user: positive or negative
+        status = request.form["status"]
+        # Already present?
+        if status == "positive":
+            column_name = "positive_branch_id"
+        else:
+            column_name = "negative_branch_id"
+        query = f"""
+            SELECT audio_url 
+            FROM branch 
+            WHERE {column_name} = '{story_id}''
         """
         results = run_query(query)
+        # if it exists: return it
         if len(results) > 0:
-            story = tuple(results[0])[0]
-        else:
-            prompt = get_prompt(input)
-            story = "new story" # call_llm(prompt)
+            audio_url = tuple(results[0])[0]
+            return {
+                "audio_url": audio_url
+            }
+        # otherwise: generate it
+        prompt = get_prompt(status)
+        story = "new story" # call_llm(prompt)
+        # Convert to audio and save in storage
+        audio_url = f"story_{story_id}_branch_{branch_id}.mp3"
+        text_to_audio(story, audio_url)
         # Store in DB in an async manner
         query = "INSERT ..."
         run_query(query)
-        # Convert to audio and save in storage
-        destination_blob_name = f"story_{story_id}_branch_{branch_id}.mp3"
-        text_to_audio(story, destination_blob_name)
+        
+        return {
+            "audio_url": audio_url
+        }
+        
         # Download audio for it to play
         return destination_blob_name
