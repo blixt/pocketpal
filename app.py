1011cae474002337c649c3950d79e3993df375b5
David Azcona <dazcona@etsy.com>
2024-06-29 15:51:15 +0100

    db call

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import json
 import os
 
 import requests
+import sqlalchemy
 # Flask
 from flask import (Flask, Response, flash, jsonify, redirect, render_template,
                    request, url_for)
 from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
+
+from db import get_query_results
 
 # APP
 app = Flask(__name__)
@@ -24,6 +27,15 @@ def index():
 
 
 # Name
-@app.route("/<name>")
-def name(name):
+@app.route("/hello/<name>")
+def hello(name):
     return f"Hello {name}!"
+
+
+# Name
+@app.route("/ratings")
+def ratings():
+    query = "SELECT name FROM ratings"
+    results = get_query_results(query)
+    results = [tuple(row) for row in results]
+    return jsonify(results)
--- db.py
+++ db.py
@@ -1,8 +1,7 @@
 import os
 
-# pip install "cloud-sql-python-connector[pg8000]" 
-from google.cloud.sql.connector import Connector
 import sqlalchemy
+from google.cloud.sql.connector import Connector
 
 # See Notion for environment variables
 DB_PASS = os.environ.get("DB_PASS")
@@ -10,27 +9,32 @@ DB_USER = os.environ.get("DB_USER")
 DB_NAME = os.environ.get("DB_NAME")
 INSTANCE_CONNECTION_NAME = os.environ.get("INSTANCE_CONNECTION_NAME")
 
-connector = Connector()
 
-def getconn():
-    conn = connector.connect(
-        INSTANCE_CONNECTION_NAME,
-        "pg8000",
-        user=DB_USER,
-        password=DB_PASS,
-        db=DB_NAME
+def get_query_results(query: str):
+    """Get query results"""
+
+    connector = Connector()
+
+    def getconn():
+        connection = connector.connect(
+            INSTANCE_CONNECTION_NAME,
+            "pg8000",
+            user=DB_USER,
+            password=DB_PASS,
+            db=DB_NAME
+        )
+        return connection
+
+    pool = sqlalchemy.create_engine(
+        "postgresql+pg8000://",
+        creator=getconn,
     )
-    return conn
 
-pool = sqlalchemy.create_engine(
-    "postgresql+pg8000://",
-    creator=getconn,
-)
+    with pool.connect() as db_conn:
+        results = db_conn.execute(
+            sqlalchemy.text(query)
+        ).fetchall()
 
-with pool.connect() as db_conn:
-    results = db_conn.execute(sqlalchemy.text("SELECT * FROM ratings")).fetchall()
+    connector.close()
 
-    for row in results:
-        print(row)
-
-connector.close()
+    return results
